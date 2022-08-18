f1a8382f4e0f6efad4877814cdf102ef56d4488c
Aochi <36964527+7ez@users.noreply.github.com>
2022-08-18 02:05:46 +0200

    h fgy6rt57u64u86u6u8453456 7m34567m5rn645nn456 n456n456n456b4545v6456b54vv456v654v456v645v456v645v645v6b455v645v64v645v645v645v456v645v645v645v645v645v645v456v645c53453x4cc523xd4345d42d4233454ur57ujtyfhjfgyhuj
    someone come to poland with a gun and shoot me in the head please

--- functions.py
+++ functions.py
@@ -569,16 +569,20 @@ def Webhook(BeatmapId, ActionName, session):
         Logtext = "ranked"
     if ActionName == 5:
         Logtext = "loved"
+     RAPLog(session["AccountId"], f"{Logtext} the beatmap {mapa[0]} ({BeatmapId})", True)
     ingamemsg = f"[https://{UserConfig['ServerURL']}u/{session['AccountId']} {session['AccountName']}] {Logtext.lower()} the map [https://osu.ppy.sh/b/{BeatmapId} {mapa[0]}]"
     params = {"k": UserConfig['FokaKey'], "to": "#announce", "msg": ingamemsg}
     FokaMessage(params)
 
-def RAPLog(UserID=999, Text="forgot to assign a text value :/"):
+def RAPLog(UserID=999, Text="forgot to assign a text value :/", isBmap=False):
     """Logs to the RAP log."""
     Timestamp = round(time.time())
     #now we putting that in oh yea
     mycursor.execute("INSERT INTO rap_logs (userid, text, datetime, through) VALUES (%s, %s, %s, 'RealistikPanel!')", (UserID, Text, Timestamp,))
     mydb.commit()
+    # skip bmap webhook
+    if isBmap:
+        return
     #webhook time
     if UserConfig["AdminLogWebhook"] != "":
         Username = GetUser(UserID)["Username"]
